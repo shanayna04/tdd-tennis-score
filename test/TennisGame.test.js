f39bfe48fc6be872628207aee9ee1acbda27e69f
Shanayna De Ugarte <shany.de.ugarte.q@gmail.com>
2026-09-02 22:32:42 -0400

    TDD: agrega test para 30-15 (pasa sin cambios en produccion)

--- test/TennisGame.test.js
+++ test/TennisGame.test.js
@@ -15,3 +15,10 @@ test ('ambos jugadores anotan un punto: 15:15' , () => {
   game.wonPoint('Player2');
   expect(game.score()).toBe('15-15');
 });
+test('jugador 1 anota dos puntos y jugador 2 uno: 30-15' , () => {
+  const game = new TennisGame('Player1', 'Player2');
+  game.wonPoint('Player1');
+  game.wonPoint('Player2');
+  game.wonPoint('Player1');
+  expect(game.score()).toBe('30-15');
+});
